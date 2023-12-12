e6dbe6af289fa1361627d46b62c4c648afd07796
Silbye <silverxlxl9@gmail.com>
2023-12-12 15:33:33 +0100

    fixed search being case sensitive

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="top-bar">
+    <div class="container">
+      <router-link class="logo-link" to="/"
+        ><img src="./assets/logo.png" alt=""
+      /></router-link>
+      <SearchComponent />
+    </div>
+  </div>
+  <router-view />
+</template>
+<script>
+import SearchComponent from "./components/SearchComponent.vue";
+export default {
+  components: {
+    SearchComponent,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/styles/style.scss";
+</style>
---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="error-page">
+    <div class="container">
+      <div class="error-message">
+        Error - the requested page was not found :(
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ErrorPage",
+};
+</script>
